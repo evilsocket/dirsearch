d8620927d451da66387544576775d119192c1ae5
europa <eur0pa@users.noreply.github.com>
2018-04-05 12:28:48 +0200

    new features

--- cmd/dirsearch/main.go
+++ cmd/dirsearch/main.go
@@ -1,15 +1,17 @@
 // This software is a Go implementation of dirsearch by Mauro Soria
 // (maurosoria at gmail dot com) written by Simone Margaritelli
 // (evilsocket at gmail dot com).
+
 package main
 
 import (
 	"crypto/tls"
 	"flag"
 	"fmt"
+	"github.com/eur0pa/dirsearch-go"
 	"github.com/evilsocket/brutemachine"
-	"github.com/evilsocket/dirsearch"
 	"github.com/fatih/color"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
@@ -25,6 +27,7 @@ import (
 type Result struct {
 	url      string
 	status   int
+	size     int
 	location string
 	err      error
 }
@@ -42,32 +45,38 @@ var (
 	base      = flag.String("url", "", "Base URL to start enumeration from.")
 	ext       = flag.String("ext", "php", "File extension.")
 	wordlist  = flag.String("wordlist", "dict.txt", "Wordlist file to use for enumeration.")
-	consumers = flag.Int("consumers", 8, "Number of concurrent consumers.")
+	threads   = flag.Int("threads", 8, "Number of concurrent threads.")
 	only200   = flag.Bool("200only", false, "If enabled, will only display responses with 200 status code.")
 	maxerrors = flag.Uint64("maxerrors", 20, "Maximum number of errors to get before killing the program.")
+	timeout   = flag.Duration("timeout", 5*time.Second, "Timeout before killing the request.")
+	method    = flag.String("method", "GET", "Request method (HEAD / GET)")
 )
 
 func DoRequest(page string) interface{} {
 	url := strings.Replace(fmt.Sprintf("%s%s", *base, page), "%EXT%", *ext, -1)
 
+	// Do not verify certificates, do not follow redirects.
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	// Do not verify certificates, do not follow redirects.
+
 	client := &http.Client{
 		Transport: tr,
+		Timeout:   *timeout,
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		}}
 
-	// Create HEAD request with random user agent.
-	req, _ := http.NewRequest("HEAD", url, nil)
+	// Create request with random user agent.
+	req, _ := http.NewRequest(*method, url, nil)
 	req.Header.Set("User-Agent", dirsearch.GetRandomUserAgent())
+	req.Header.Set("Accept", "*/*")
 
 	if resp, err := client.Do(req); err == nil {
-		resp.Body.Close()
+		defer resp.Body.Close()
 		if resp.StatusCode == 200 || !*only200 {
-			return Result{url, resp.StatusCode, resp.Header.Get("Location"), nil}
+			content, _ := ioutil.ReadAll(resp.Body)
+			return Result{url, resp.StatusCode, len(content), resp.Header.Get("Location"), nil}
 		}
 	} else {
 		atomic.AddUint64(&errors, 1)
@@ -84,13 +93,14 @@ func OnResult(res interface{}) {
 	}
 
 	now := time.Now().Format("15:04:05")
+
 	switch {
 	// error not due to 404 response
 	case result.err != nil && result.status != 404:
 		r.Printf("[%s] [???] %s : %v\n", now, result.url, result.err)
 	// 2xx
 	case result.status >= 200 && result.status < 300:
-		g.Printf("[%s] [%d] %s\n", now, result.status, result.url)
+		g.Printf("[%s] [%d] [%d] %s\n", now, result.status, result.size, result.url)
 	// 3xx
 	case !*only200 && result.status >= 300 && result.status < 400:
 		b.Printf("[%s] [%d] %s -> %s\n", now, result.status, result.url, result.location)
@@ -111,7 +121,7 @@ func OnResult(res interface{}) {
 func main() {
 	setup()
 
-	m = brutemachine.New(*consumers, *wordlist, DoRequest, OnResult)
+	m = brutemachine.New(*threads, *wordlist, DoRequest, OnResult)
 	if err := m.Start(); err != nil {
 		panic(err)
 	}
