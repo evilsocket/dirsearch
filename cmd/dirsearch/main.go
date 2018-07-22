d558f4f930915bc96a7567a2862d8902b5546c5c
europa <eur0pa@users.noreply.github.com>
2018-07-22 21:13:17 +0200

    many balls

--- cmd/dirsearch/main.go
+++ cmd/dirsearch/main.go
@@ -24,12 +24,10 @@ import (
 	"time"
 )
 
-// Result represents the resul of each HEAD request
-// to a given URL.
 type Result struct {
 	url      string
 	status   int
-	size     int
+	size     uint64
 	location string
 	err      error
 }
@@ -37,25 +35,33 @@ type Result struct {
 var (
 	m *brutemachine.Machine
 
-	errors = uint64(0)
-
 	g = color.New(color.FgGreen)
 	y = color.New(color.FgYellow)
 	r = color.New(color.FgRed)
 	b = color.New(color.FgBlue)
 
+	errors     = uint64(0)
 	fail_codes = make(map[int]bool)
 
-	ext       = flag.String("ext", "php", "File extension.")
-	exclude   = flag.String("exclude", "404", "Status codes to exclude.")
-	method    = flag.String("method", "GET", "Request method (HEAD / GET)")
-	base      = flag.String("url", "", "Base URL to start enumeration from.")
-	wordlist  = flag.String("wordlist", "dict.txt", "Wordlist file to use for enumeration.")
-	maxerrors = flag.Uint64("maxerrors", 20, "Maximum number of errors to get before killing the program.")
-	timeout   = flag.Uint("timeout", 5, "Timeout before killing the request.")
-	wildcard  = flag.Bool("wild", false, "Test and skip wildcard responses.")
-	only200   = flag.Bool("200only", false, "If enabled, will only display responses with 200 status code.")
-	threads   = flag.Int("threads", 8, "Number of concurrent threads.")
+	transport = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	httpClient = &http.Client{
+		Transport: transport,
+	}
+
+	ext       = flag.String("e", "php", "File extension")
+	exclude   = flag.String("x", "404", "Status codes to exclude")
+	method    = flag.String("m", "GET", "Request method (HEAD / GET)")
+	base      = flag.String("u", "", "URL to enumerate")
+	wordlist  = flag.String("w", "dict.txt", "Wordlist file")
+	maxerrors = flag.Uint64("E", 20, "Max. errors before exiting")
+	timeout   = flag.Uint("T", 10, "Timeout before killing the request")
+	wildcard  = flag.Bool("sw", false, "Skip wildcard responses")
+	only200   = flag.Bool("2", false, "If enabled, will only display responses with 200 status code.")
+	follow    = flag.Bool("f", false, "Follow redirects.")
+	threads   = flag.Int("t", 10, "Number of concurrent threads.")
 )
 
 func IsWildcard(url string) bool {
@@ -66,7 +72,7 @@ func IsWildcard(url string) bool {
 		return false
 	}
 
-	if res.status == 200 || fail_codes[res.status] {
+	if res.status == 200 || !fail_codes[res.status] {
 		*wildcard = false
 		return true
 	}
@@ -76,38 +82,26 @@ func IsWildcard(url string) bool {
 }
 
 func DoRequest(page string) interface{} {
-	if errors > *maxerrors {
-		r.Fprintf(os.Stderr, "\nExceeded %d errors, quitting ...", *maxerrors)
-		os.Exit(1)
-	}
-
 	url := strings.Replace(fmt.Sprintf("%s%s", *base, page), "%EXT%", *ext, -1)
 
-	// Do not verify certificates, do not follow redirects.
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	client := &http.Client{
-		Transport: tr,
-		Timeout:   time.Duration(*timeout) * time.Second,
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		}}
-
-	// Create request with random user agent.
 	req, _ := http.NewRequest(*method, url, nil)
+
 	req.Header.Set("User-Agent", dirsearch.GetRandomUserAgent())
 	req.Header.Set("Accept", "*/*")
 
-	if resp, err := client.Do(req); err == nil {
+	resp, err := httpClient.Do(req)
+	if resp != nil {
 		defer resp.Body.Close()
-		content, _ := ioutil.ReadAll(resp.Body)
-		if (!fail_codes[resp.StatusCode] && !*only200) || *wildcard == true {
-			return Result{url, resp.StatusCode, len(content), resp.Header.Get("Location"), nil}
-		}
-	} else {
+	}
+	if err != nil {
 		atomic.AddUint64(&errors, 1)
+		return Result{url, 0, uint64(0), "", err}
+	}
+
+	content, _ := ioutil.ReadAll(resp.Body)
+
+	if (!fail_codes[resp.StatusCode] && !*only200) || *wildcard == true {
+		return Result{url, resp.StatusCode, uint64(len(content)), resp.Header.Get("Location"), nil}
 	}
 
 	return nil
@@ -124,39 +118,40 @@ func OnResult(res interface{}) {
 	now := time.Now().Format("15:04:05")
 
 	switch {
+
 	// error not due to 404 response
 	case result.err != nil && result.status != 404:
-		r.Printf("[%s] ........ %s : %v\n", now, result.url, result.err)
+		r.Printf("[%s] %s : %v\n", now, result.url, result.err)
+
 	// 2xx
 	case result.status >= 200 && result.status < 300:
 		if *method == "GET" {
-			g.Printf("[%s] %-3d %-8d %s\n", now, result.status, result.size, result.url)
+			g.Printf("[%s] %-3d %-9d %s\n", now, result.status, result.size, result.url)
 		} else {
 			g.Printf("[%s] %-3d %s\n", now, result.status, result.url)
 		}
+
 	// 3xx
 	case !*only200 && result.status >= 300 && result.status < 400:
-		b.Printf("[%s] %-3d %-8d %s -> %s\n", now, result.status, result.size, result.url, result.location)
+		b.Printf("[%s] %-3d %-9d %s -> %s\n", now, result.status, result.size, result.url, result.location)
+
 	// 4xx
 	case result.status >= 400 && result.status < 500 && result.status != 404:
-		y.Printf("[%s] %-3d %-8d %s\n", now, result.status, result.size, result.url)
+		y.Printf("[%s] %-3d %-9d %s\n", now, result.status, result.size, result.url)
+
 	// 5xx
 	case result.status >= 500 && result.status < 600:
-		r.Printf("[%s] %-3d %-8d %s\n", now, result.status, result.size, result.url)
+		r.Printf("[%s] %-3d %-9d %s\n", now, result.status, result.size, result.url)
 	}
 
+	if errors > *maxerrors {
+		r.Fprintf(os.Stderr, "\nExceeded %d errors, quitting ...", *maxerrors)
+		os.Exit(1)
+	}
 }
 
 func main() {
 	setup()
-
-	// check for wildcard responses, and return if true
-	if *wildcard == true {
-		if IsWildcard(*base) == true {
-			r.Fprintf(os.Stderr, "\nWildcard detected on %s, skipping....\n", *base)
-			os.Exit(0)
-		}
-	}
 
 	// create a list of exclusions
 	if *exclude != "" {
@@ -166,6 +161,25 @@ func main() {
 		}
 	}
 
+	// set timeout
+	httpClient.Timeout = time.Duration(*timeout) * time.Second
+
+	// set redirects policy
+	if !*follow {
+		httpClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+	}
+
+	// check for wildcard responses, and return if true
+	if *wildcard == true {
+		if IsWildcard(*base) == true {
+			r.Fprintf(os.Stderr, "\nWildcard detected on %s, skipping....\n", *base)
+			os.Exit(0)
+		}
+	}
+
+	// start
 	m = brutemachine.New(*threads, *wordlist, DoRequest, OnResult)
 
 	if err := m.Start(); err != nil {
@@ -181,7 +195,7 @@ func main() {
 
 // Do some initialization.
 // NOTE: We can't call this in the 'init' function otherwise
-// flags are gonna be mandatory for unit test modules.
+// *are gonna be mandatory for unit test modules.
 func setup() {
 	flag.Parse()
 
